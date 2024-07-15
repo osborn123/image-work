54abd856b54c9530322a6166914694c1bc7f7e84
osborn123 <s2420193@ed.ac.uk>
2024-07-15 18:55:30 +0100

    fix zf

--- SP_LP_CV/generate_feature/test_image_classification.py
+++ SP_LP_CV/generate_feature/test_image_classification.py
@@ -23,42 +23,86 @@ train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
 class ZFNet(nn.Module):
-    def __init__(self, num_classes=10):
-        super(ZFNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 96, kernel_size=7, stride=2, padding=1)
-        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.conv2 = nn.Conv2d(96, 256, kernel_size=5, stride=2, padding=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.conv3 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1)
-        self.conv5 = nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1)
-        self.pool5 = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(1024, num_classes)
+    """ A implementation of the ZFNet architecture from the paper 'Visualizing and 
+        Understanding Convolutional Networks' by Zeiler and Fergus"""
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 96, kernel_size=7, stride=2, padding=3, padding_mode='reflect')
+        self.conv2 = nn.Conv2d(96, 256, kernel_size=5, stride=2, padding=2, padding_mode='reflect')
+        self.conv3 = nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        self.conv4 = nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        self.conv5 = nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        self.fc6 = nn.Linear(8*8*256, 4096)  # 修改这里
+        self.fc7 = nn.Linear(4096, 4096)
+        self.fc8 = nn.Linear(4096, 1000)
+        self.pool1 = nn.MaxPool2d(3, stride=2)
+        self.pool2 = nn.MaxPool2d(3, stride=2)
+        self.drop = nn.Dropout(0.5)
+        self.lrn = nn.LocalResponseNorm(size=5, alpha=10e-4, beta=0.75, k=2.0)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.pool5(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
+        x = self.lrn(self.pool1(F.relu(self.conv1(x))))
+        
+        x = self.lrn(self.pool2(F.relu(self.conv2(x))))
+       
+        x = F.relu(self.conv3(x))
+        
+        x = F.relu(self.conv4(x))
+        
+        x = self.pool2(F.relu(self.conv5(x)))
+       
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.drop(self.fc6(x)))
+        x = F.relu(self.drop(self.fc7(x)))
+        x = self.fc8(x)
         return x
     
     def generate_feature(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.pool5(x)
-        x = torch.flatten(x, 1)
+        x = self.lrn(self.pool1(F.relu(self.conv1(x))))
+        x = self.lrn(self.pool2(F.relu(self.conv2(x))))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool2(F.relu(self.conv5(x)))
+        x = x.view(x.size(0), -1)
         return x
+# class ZFNet(nn.Module):
+#     def __init__(self, num_classes=10):
+#         super(ZFNet, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 96, kernel_size=7, stride=2, padding=1)
+#         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+#         self.conv2 = nn.Conv2d(96, 256, kernel_size=5, stride=2, padding=1)
+#         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+#         self.conv3 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+#         self.conv4 = nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1)
+#         self.conv5 = nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1)
+#         self.pool5 = nn.AdaptiveAvgPool2d((1, 1))
+#         self.fc = nn.Linear(1024, num_classes)
+
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.pool1(x)
+#         x = self.conv2(x)
+#         x = self.pool2(x)
+#         x = self.conv3(x)
+#         x = self.conv4(x)
+#         x = self.conv5(x)
+#         x = self.pool5(x)
+#         x = torch.flatten(x, 1)
+#         x = self.fc(x)
+#         return x
+    
+#     def generate_feature(self, x):
+#         x = self.conv1(x)
+#         x = self.pool1(x)
+#         x = self.conv2(x)
+#         x = self.pool2(x)
+#         x = self.conv3(x)
+#         x = self.conv4(x)
+#         x = self.conv5(x)
+#         x = self.pool5(x)
+#         x = torch.flatten(x, 1)
+#         return x
 
 
 def ResidualBlock(in_channels, out_channels, stride=1):
